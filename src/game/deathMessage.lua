b6afee673796d2c4345a73621fc8c52db67cfa9a
Sceat <sceat@hydre.io>
2019-08-10 15:25:53 +0300

    upd

--- src/game/deathMessage.lua
+++ src/game/deathMessage.lua
@@ -2,6 +2,10 @@ require('src.utils.msg')
 
 script.on_event(defines.events.on_entity_died, function(event)
 	if event.entity.type == 'player' then
-		say((event.entity.player.name or 'undefined').." s'est fait peter la gueule par "..(event.cause.player.name or event.cause.name or 'undefined'))
+		if event.cause ~= nil and event.cause.type == 'player' then
+			say((event.entity.player.name or 'undefined').." s'est fait peter la gueule par "..(event.cause.player.name or 'undefined'))
+		else
+			say((event.entity.player.name or 'undefined').." s'est fait peter la gueule par "..(event.cause.name or 'undefined'))
+		end
 	end
 end)
