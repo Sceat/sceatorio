f1b38672ff18fa62f2592a5c0c44985994fef6d8
sceat <fetch@sceat.xyz>
2021-06-15 21:23:31 +0200

    sceatorio:1.1.2 release

--- release.sh
+++ release.sh
@@ -21,8 +21,8 @@ echo "
     "author": "Sceat",
     "contact": "@Sceat_",
     "homepage": "",
-    "factorio_version": "1.1.0",
-    "dependencies": ["base >= 1.1.0"],
+    "factorio_version": "1.1",
+    "dependencies": ["base >= 1.1.21"],
     "description": "Separated spawn for a vanilla multiplayer experience, play with your friends without worrying for grief or resources stealing while offline"
 }
 EOM
